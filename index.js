bd075fea6bf82371e77052fb029553d4087cbd56
Rodriguez fennema lucas <ld.fennema@gmail.com>
2020-01-21 20:21:53 -0300

    crud api task con metodos funcionando

--- index.js
+++ index.js
@@ -21,11 +21,19 @@ const connection = mongoose.connection;
 
 
 app.get('/task', (req, res)=>{
-})
+    Task.find({}, (err, tasks) =>{
+        if( err) return res.status(500).send({message:`Error al realizar la busqueda ${err}`});
+        if (!tasks)  return res.status(404).send({message:`Error no se encontraron tareas `});
+        res.send(200,{tasks})
+    });
+});
 app.get('/task/:taskId', (req, res)=> {
-    console.log('get /TASKid');
-    console.log(req.body);
-
+    let tid= req.params.taskId;
+    Task.findById(tid,(err,taskfind)=>{
+        if( err) return res.status(500).send({message:`Error al realizar la busqueda ${err}`});
+        if (!taskfind)  return res.status(404).send({message:`Error no se encontro la tarea con esa id ${tid}`});
+       res.status(200).send({taskfind});
+    });
 })
 app.post('/task', (req, res)=>{
 
@@ -36,19 +44,32 @@ app.post('/task', (req, res)=>{
     task.status = 'pendiente' ;
 
     task.save((err, taskStored) =>{
-        if(err) {
-            res.status(500).send({message: `Error al guardar en la db ${err}`})
-             return;}
-        
+        if(err) 
+            return res.status(500).send({message: `Error al guardar en la db ${err}`});        
         res.status(200).send({task: taskStored});
     });
 });
 app.put('/task/:taskId', (req, res)=>{
-})
+    let tid = req.params.taskId;
+    let updatetask = req.body;
 
+    Task.findByIdAndUpdate(tid, updatetask, (err,taskupdate) => {
+        if( err) return res.status(500).send({message:`Error al actualizar la busqueda ${err}`});
+
+        res.status(200).send({updatetask});
+    });
+});
 app.delete('/task/:taskId', (req, res) =>{
-
-})
+    let tid= req.params.taskId;
+    Task.findById(tid,(err,taskrm)=>{
+        if( err) return res.status(500).send({message:`Error al realizar la busqueda ${err}`});
+        taskrm.remove(err =>{
+            if( err) return res.status(500).send({message:`Error al borrar la busqueda ${err}`});
+            res.status(200).send({message:"La tarea se borro correctamente"});    
+        });
+       
+    });
+});
 connection.once('open', ()=>{
     console.log("MOngoDB database connection established ssfly");
 
