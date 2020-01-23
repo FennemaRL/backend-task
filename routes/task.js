d70cdb01646229860c0906326933888e998c0d44
Rodriguez fennema lucas <ld.fennema@gmail.com>
2020-01-23 14:21:01 -0300

    arreglo ruta caso id no encontrada

--- routes/task.js
+++ routes/task.js
@@ -9,47 +9,50 @@ router.route('/').get((req,res) => { /*tareas*/
 router.route('/:taskId').get((req,res) =>{/*tarea de id: */
     let tid= req.params.taskId;
     Task.findById(tid)
-        .then(task => res.status(200).json(task))
+        .then(task =>
+            { tasknotfind(res,tid,task);
+              res.status(200).json(task);})
         .catch(err  => res.status(400).json(`Error al buscar la tarea ${tid} : `+err));
 
 });
-
 router.route('/').post((req, res)=>{ /*nueva tarea*/
     let task = new Task();
     task.name = req.body.name ;
     task.estimatedTime = Number(req.body.estimatedTime) ;
     task.proyect = req.body.proyect ;
     task.status = 'pendiente' ;
-
-    tasksave(req, res, task);
+    tasksave( res, task);
 
 });
 router.route('/:taskId').put((req, res)=>{ /*modificar tarea*/
     let tid= req.params.taskId;
     Task.findById(tid)
         .then( task   => {
+            tasknotfind(res,tid,task)
             task.name = req.body.name;
             task.estimatedTime = Number(req.body.estimatedTime) ;
             task.proyect = req.body.proyect ;
-
-            tasksave(req, res, task);
+            tasksave(res, task);
 
         })
-        .catch(err => {res.status(400).json( `Error al borrar en la db ${err}`); console.log(err); });
+        .catch(err => res.status(400).json( `Error al borrar en la db ${err}`));
 
 });
 router.route('/:taskId').delete((req, res)=>{ /*borrar tarea*/
     let tid= req.params.taskId;
     Task.findByIdAndDelete(tid)
         .then(()   => res.status(200).json("Se elimino con exito"))
-        .catch(err =>{ res.status(400).json( `Error al borrar en la db ${err}`); console.log(err); });
+        .catch(err => res.status(400).json( `Error al borrar en la db ${err}`));
 
 });
+const tasknotfind = (res, taskId, task) =>{
+    if(!task) return res.status(404).json( `Error no se contro la tarea con id : ${taskId}`);
+}
 
-function tasksave(req, res, task){
+function tasksave(res, task){
     task.save()
     .then(()   => res.status(200).json(task))
     .catch(err => res.status(400).json( `Error al guardar en la db ${err}`));
-}
 
+}
 module.exports = router;
